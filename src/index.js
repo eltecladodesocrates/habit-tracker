e50a33cdb3ef431320a427a2b561ffc9e61db060
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-06-15 11:54:27 -0400

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class HabitTrackerApp extends React.Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  constructor(props) {
+    super(props)
+    this.handleAddHabit = this.handleAddHabit.bind(this)
+    this.state = {
+      habits: []
+    }
+  }
+
+  handleAddHabit(habit) {
+    this.setState((prevState) => ({ habits: prevState.habits.concat(habit) }))
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Habits habits={this.state.habits} />
+        <AddHabit
+          handleAddHabit={this.handleAddHabit}
+        />
+      </div>
+    )
+  }
+}
+
+const Header = () => (
+  <div>
+    <h1>Habit Tracker</h1>
+  </div>
+)
+
+const Habits = (props) => (
+  <div>
+    {props.habits.map((habit) => <Habit key={habit} habiText={habit}></Habit>)}
+  </div>
+)
+
+const Habit = (props) => (
+  <div>
+    {props.habiText}
+    <input type="checkbox" />
+    <input type="checkbox" />
+    <input type="checkbox" />
+    <button>R</button>
+  </div>
+)
+
+class AddHabit extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.handleAddHabit = this.handleAddHabit.bind(this)
+  }
+
+  handleAddHabit(e) {
+    e.preventDefault()
+    const newHabit = e.target.elements.habit.value
+    this.props.handleAddHabit(newHabit)
+    e.target.elements.habit.value = ''
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleAddHabit}>
+        <input type="text" name='habit' autoComplete='off' />
+        <button>Add Habit</button>
+      </form>
+    )
+  }
+}
+
+
+ReactDOM.render(<HabitTrackerApp />, document.getElementById('root'))
